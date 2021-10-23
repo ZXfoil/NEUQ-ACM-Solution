356a2d7393db57882775983bc3f5162bb3d1c1ef
ZXfoil <42538973+ZXfoil@users.noreply.github.com>
2021-10-23 23:19:33 +0800

    Create 7-1.cpp
    #include <bits/stdc++.h>
    using namespace std;
    
    const int maxn=1e6+5;
    
    int N,len,L;
    char txt[maxn],ms[maxn];
    
    int find() {
        int Ans=0;
        L=strlen(ms+1);
    //     printf("%d %d\n",len,L);
        for (int i=1; i+L-1<=len; ++i) {
    //        printf("\t%d\n",i);
            bool flg=1;
            for (int j=1; j<=L; ++j) if (txt[i+j-1]!=ms[j]) {flg=0;break;}
            Ans+=flg;
    //        if (flg) printf("%d\n",i);
        }
        return Ans;
    }
    
    int main() {
        scanf("%d\n%s",&N,txt+1),len=strlen(txt+1);
        for (int i=1; i<=N; ++i) {
            scanf("%s",ms+1);
            printf("%d\n",find());
        }
        return 0;
    }

---
+++ week1/祝向博/7-1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn=1e6+5;
+
+int N,len,L;
+char txt[maxn],ms[maxn];
+
+int find() {
+    int Ans=0;
+    L=strlen(ms+1);
+//     printf("%d %d\n",len,L);
+    for (int i=1; i+L-1<=len; ++i) {
+//        printf("\t%d\n",i);
+        bool flg=1;
+        for (int j=1; j<=L; ++j) if (txt[i+j-1]!=ms[j]) {flg=0;break;}
+        Ans+=flg;
+//        if (flg) printf("%d\n",i);
+    }
+    return Ans;
+}
+
+int main() {
+    scanf("%d\n%s",&N,txt+1),len=strlen(txt+1);
+    for (int i=1; i<=N; ++i) {
+        scanf("%s",ms+1);
+        printf("%d\n",find());
+    }
+    return 0;
+}
